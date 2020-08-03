1407cc77db5dd2837f6a1a96c2bf4f2bbf870ce4
shrijan <shrijan00003@gmail.com>
2020-08-04 00:04:03 +0545

    - security page added - design cope with figma for footer container and stuff

---
+++ src/website/views/security.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import WebsiteLayout from 'website/website.layout';
+
+const Security = () => {
+  return (
+    <WebsiteLayout>
+      <>
+        <div className='mm-container-right'>
+          <div className='row'>
+            <div className='col-lg-6'>this is text area</div>
+            <div className='col-lg-6 text-right'>this is image area</div>
+          </div>
+        </div>
+      </>
+    </WebsiteLayout>
+  );
+};
+
+export default Security;
--- src/website/website.route.tsx
+++ src/website/website.route.tsx
@@ -2,11 +2,11 @@ import React from 'react';
 import { Switch, Redirect, Route } from 'react-router-dom';
 
 import Website from './website';
-import { NotFound, Notice, Privacy, TermNService } from './views';
+import { NotFound, Notice, Privacy, TermNService, Security } from './views';
 import { WebsiteProvider } from './website.context';
 import { websiteRouteConstant } from './website.constant';
 
-const { DEF, NOTICE, NOT_FOUND, PRIVACY, TERMS } = websiteRouteConstant;
+const { DEF, NOTICE, NOT_FOUND, PRIVACY, TERMS, SECURITY } = websiteRouteConstant;
 
 function WebsiteRoute() {
   return (
@@ -16,6 +16,7 @@ function WebsiteRoute() {
         <Route exact path={NOTICE} component={Notice} />
         <Route exact path={PRIVACY} component={Privacy} />
         <Route exact path={TERMS} component={TermNService} />
+        <Route exact path={SECURITY} component={Security} />
         <Route exact path={NOT_FOUND} component={NotFound} />
         <Redirect to={NOT_FOUND} />
       </Switch>
