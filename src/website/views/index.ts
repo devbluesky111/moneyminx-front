8939f014b9d7270c6107dc3460d4671e023354df
shrijan <shrijan00003@gmail.com>
2020-08-08 21:44:34 +0545

    mm-frontend#11: basic design for pricing page

--- src/website/views/index.ts
+++ src/website/views/index.ts
@@ -1,5 +1,6 @@
 export { default as Notice } from './notice';
 export { default as Privacy } from './privacy';
+export { default as Pricing } from './pricing';
+export { default as Security } from './security';
 export { default as NotFound } from './not-found';
 export { default as TermNService } from './term-service';
-export { default as Security } from './security';
