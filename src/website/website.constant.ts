8272509581cc37c892d0f4b2846e9bbabd4956b2
shrijan <shrijan00003@gmail.com>
2020-08-10 00:02:20 +0545

    mm-frontend#12: about us page basic ui completed, image switching will be done with js

--- src/website/website.constant.ts
+++ src/website/website.constant.ts
@@ -1,9 +1,10 @@
 export const websiteRouteConstant = {
   DEF: '/w',
   TERMS: '/w/terms',
+  ABOUT: '/w/about',
   NOT_FOUND: '/w/404',
   NOTICE: '/w/notice',
   PRIVACY: '/w/privacy',
-  SECURITY: '/w/security',
   PRICING: '/w/pricing',
+  SECURITY: '/w/security',
 };
