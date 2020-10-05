31c11588c634c305e03a80fb7840c1da43285ef2
sagar <sagarlimbu787@gmail.com>
2020-10-05 13:35:20 +0545

    account design WIP

--- src/account/views/account.tsx
+++ src/account/views/account.tsx
@@ -1,8 +1,18 @@
 import React from 'react';
 import { AccountProps } from '../account.type';
+import AccountTable from './account-table';
+import Button from 'react-bootstrap/Button';
 
 const Account: React.FC<AccountProps> = () => {
-  return <div>we are in Account page</div>;
+  return (
+    <div className='mm-account'>
+      {/* <NavBarSection /> */}
+      {/* <AllocationSubNavigation /> */}
+      <Button variant="primary" className='w-100 mb-4'>Add Position</Button>
+      <AccountTable />
+      {/* <FooterSection /> */}
+    </div>
+  );
 };
 
 export default Account;
