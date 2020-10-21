c9607d663241282bcadded165788ca04381df4a8
hussein <h.yahfoufi@gmail.com>
2020-10-20 20:03:52 -0700

    Closes #248
    Copied Net Worth header to common/app.header.tsx
    
    Used that header in settings, allocation and net worth screens.
    
    Please also use it in the Account Details screen when ready

--- src/allocation/views/allocation.tsx
+++ src/allocation/views/allocation.tsx
@@ -1,18 +1,19 @@
 import React, { useState } from 'react';
 
+import AppHeader from 'common/app.header';
 import useAllocation from 'allocation/hooks/useAllocation';
 import { AllocationsFilter } from 'allocation/allocation.enum';
 import CircularSpinner from 'common/components/spinner/circular-spinner';
 
 import { AllocationProps } from '../allocation.type';
 import AllocationOverview from './allocation-overview';
-import NavBarSection from '../../setting/inc/setting.header';
 import FooterSection from '../../setting/inc/setting.footer';
 import AllocationSubNavigation from './allocation-sub-navigation';
 
 const Allocation: React.FC<AllocationProps> = () => {
   const [filter, setFilter] = useState(AllocationsFilter.TYPE);
   const { fetching, allocations, error, allocationChartData } = useAllocation(filter);
+  const [openNav, setOpenNav] = useState<boolean>(false);
 
   if (fetching || error || !allocations || !allocationChartData) {
     return <CircularSpinner />;
@@ -24,7 +25,7 @@ const Allocation: React.FC<AllocationProps> = () => {
 
   return (
     <div className='mm-setting mm-allocation'>
-      <NavBarSection />
+      <AppHeader toggleMenu={() => setOpenNav(!openNav)} />
       <AllocationSubNavigation onTypeChange={handleTypeChange} filter={filter} />
       <AllocationOverview allocations={allocations} chartData={allocationChartData} filter={filter} />
       <FooterSection />
---
+++ src/common/app.header.tsx
@@ -0,0 +1,65 @@
+import React from 'react';
+import { Link, useLocation } from 'react-router-dom';
+
+import Logo from 'assets/icons/logo.svg';
+import { useAuthState } from 'auth/auth.context';
+import { capitalize } from 'common/common-helper';
+// TODO Badge depends on the plan level
+import ProBadge from 'assets/images/networth/pro-badge.svg';
+import DefaultAvatar from 'assets/icons/default-avatar.svg';
+
+interface AppHeaderProps {
+  toggleMenu: () => void;
+}
+const AppHeader: React.FC<AppHeaderProps> = ({ toggleMenu }) => {
+  const { user } = useAuthState();
+  const { pathname } = useLocation();
+
+
+  const navClass = (label: string) => (pathname.includes(label) ? 'mm-app-nav-item active' : 'mm-app-nav-item');
+
+  return (
+    <header>
+      <nav className='navbar navbar-expand-lg money-minx-header'>
+        <div className='container'>
+          <button
+            className='navbar-toggler collapsed'
+            type='button'
+            data-toggle='collapse'
+            data-target='#headerMenu'
+            aria-expanded='false'
+          >
+            <span className='navbar-toggler-icon' />
+          </button>
+          <Link to='/net-worth' className='navbar-brand'>
+            <img src={Logo} alt='Money Minx logo' />
+          </Link>
+          <div className='headtab'>
+            <Link to='/net-worth' className={navClass('net-worth')}>
+              Net Worth
+            </Link>
+            <Link to='/allocation' className={navClass('allocation')}>Allocation</Link>
+          </div>
+          <div className='head-right'>
+            <button type='button' className='upgrader-btn' data-toggle='modal' data-target='#upgradeModal'>
+              Upgrade
+            </button>
+            <div className='badge-box'>
+              <img src={ProBadge} alt='Pro badge' />
+            </div>
+            <div className='btn-group'>
+              <button type='button' className='profile-toggle' onClick={toggleMenu}>
+                <span>
+                  <img src={user?.picture || DefaultAvatar} alt='Profile avatar' />
+                </span>
+                <span>{capitalize(user?.firstName || 'User')}</span>
+              </button>
+            </div>
+          </div>
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default AppHeader;
