c8446177d7ac505a99826b3f8a0a3e829d8cbe3f
hussein <h.yahfoufi@gmail.com>
2020-10-22 06:41:07 -0700

    updates to modal UI

--- src/setting/inc/subscription-cancel.modal.tsx
+++ src/setting/inc/subscription-cancel.modal.tsx
@@ -17,17 +17,17 @@ const SubscriptionCancelModal: React.FC<Props> = ({ subscriptionCancelModal, sub
         <p>
           Your account will remain active until <span>{endingDate}</span>. Please let us know why you decided to cancel
           and if there is anything we can do to improve our service by emailing{' '}
-          <span className='link'>hello@moneyminx.com</span>.
+          <a href='mailto:hello@moneyminx.com' className=''>hello@moneyminx.com</a>.
         </p>
         <div className='modal-btn-wrapper'>
           <button
-            className='mm-btn bg-primary mm-btn-primary-outline text-white'
+            className='mm-btn-animate mm-btn-primary'
             onClick={() => subscriptionCancelModal.close()}
           >
             Never mind
           </button>
-          <div className='create-new-btn text-center'>
-            <Link to='/dashboard'>Confirm Cancellation</Link>
+          <div className='text-center'>
+            <Link className='link-gray' to='/dashboard'>Confirm Cancellation</Link>
           </div>
         </div>
       </div>
