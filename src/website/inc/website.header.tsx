5fd9a30ba3f9e6ed3e91d78dcbfb4f433bc032e0
sagar <sagarlimbu787@gmail.com>
2020-10-25 12:55:02 +0545

    develop side bar menu for mobile view

--- src/website/inc/website.header.tsx
+++ src/website/inc/website.header.tsx
@@ -2,6 +2,9 @@ import React, { useState } from 'react';
 import useSize from 'common/hooks/useSize';
 import { Link, useLocation } from 'react-router-dom';
 import { ReactComponent as Logo } from 'assets/icons/logo.svg';
+import { ReactComponent as Beta } from 'assets/icons/beta.svg';
+import { ReactComponent as Cross } from 'assets/icons/cross.svg';
+import { ReactComponent as MoneyMinxLogoMobile } from 'assets/icons/money-minx-logo-mobile.svg';
 
 interface LeftMenuType {
   all: string;
@@ -42,6 +45,10 @@ const WebsiteHeader = () => {
         <div className={`${expand === 'all' ? 'expand-all' : ''}`}>
           <div className={`menu-btn-wrapper`}>
             <div className={`menu-list-wrapper ${expand === 'menus' ? 'expand-menu' : ''}`}>
+              <div className='mm-navbar-logo'>
+                <MoneyMinxLogoMobile className='mr-3' />
+                <Beta />
+              </div>
               <ul className='navbar-nav mr-auto navbar-menu-list'>
                 <li className={navClass('pricing')}>
                   <Link className='mm-nav-link' to='/pricing'>
@@ -64,9 +71,19 @@ const WebsiteHeader = () => {
                     About
                   </Link>
                 </li>
+                <li className={navClass('login')}>
+                  <Link className='mm-nav-link' to='login'>
+                    Log In
+                  </Link>
+                </li>
+                <li className='mm-nav-item'>
+                  <Link className='mm-nav-link' to='/signup'>
+                    <button className='w-100 mm-btn-signup btn-outline-primary mm-btn-animate'>Sign Up</button>
+                  </Link>
+                </li>
               </ul>
             </div>
-            <div className='auth-btn-wrapper mr-1'>
+            {/* <div className='auth-btn-wrapper mr-1'>
               <ul className='navbar-nav'>
                 <li className={navClass('login')}>
                   <Link className='mm-nav-link' to='login'>
@@ -79,7 +96,7 @@ const WebsiteHeader = () => {
                   </Link>
                 </li>
               </ul>
-            </div>
+            </div> */}
           </div>
         </div>
       </div>
