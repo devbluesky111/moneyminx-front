12dba8c0fce840e604227b969de9e0be09715834
shrijan <shrijan00003@gmail.com>
2020-10-28 16:19:34 +0545

    mm-frontend#142: - chart upload functionlity added - chart share with media - copy image to clipboard - download image functionlity concluded

--- src/allocation/modal/chart-share-modal.tsx
+++ src/allocation/modal/chart-share-modal.tsx
@@ -1,16 +1,17 @@
-import React, { useEffect, useState } from 'react';
-import { Modal, ModalType } from 'common/components/modal';
-import { ReactComponent as Logo } from 'assets/icons/logo.svg';
+import React, { useEffect, useRef, useState } from 'react';
 import { FacebookShareButton, PinterestShareButton, TwitterShareButton } from 'react-share';
 
 import { useAuthState } from 'auth/auth.context';
+import { postUploadChart } from 'api/request.api';
+import { Modal, ModalType } from 'common/components/modal';
 import useFileDownload from 'common/hooks/useFileDownload';
 import DefaultAvatar from 'assets/icons/default-avatar.svg';
+import { ReactComponent as Logo } from 'assets/icons/logo.svg';
 import { ReactComponent as TwitterIcon } from 'assets/icons/twitter.svg';
-import { ReactComponent as FacebookIcon } from 'assets/icons/facebook.svg';
-import { ReactComponent as PinterestIcon } from 'assets/icons/pinterest.svg';
 import useAllocationSetting from 'allocation/hooks/useAllocationSetting';
 import CircularSpinner from 'common/components/spinner/circular-spinner';
+import { ReactComponent as FacebookIcon } from 'assets/icons/facebook.svg';
+import { ReactComponent as PinterestIcon } from 'assets/icons/pinterest.svg';
 
 interface ChartShareModalProps {
   chartShareModal: ModalType;
@@ -18,23 +19,89 @@ interface ChartShareModalProps {
   chatLegendComponent: React.ReactNode;
 }
 
+export enum ShareTypeEnum {
+  PINTEREST = 'PINTEREST',
+  FACEBOOK = 'FACEBOOK',
+  TWITTER = 'TWITTER',
+}
+
 const ChartShareModal: React.FC<ChartShareModalProps> = ({ chartShareModal, chartComponent, chatLegendComponent }) => {
-  const { df, getImage } = useFileDownload();
   const { user } = useAuthState();
-  const [blob, setBlob] = useState<any>(null);
+  const { df, getImage } = useFileDownload();
+  const [imageUrl, setImageUrl] = useState<string>();
+  const [copySuccess, setCopySuccess] = useState(false);
 
   const { loading, settings }: { loading: boolean; settings: any } = useAllocationSetting();
 
-  const getBlobImage = async () => {
-    if (!blob) {
-      const { image, error } = await getImage('allocation-share-card');
-      if (!error) {
-        setBlob(image);
+  const pinterestButtonRef = useRef<any>(null);
+  const twitterShareButtonRef = useRef<any>(null);
+  const facebookShareButtonRef = useRef<any>(null);
+
+  useEffect(() => {
+    if (copySuccess) {
+      setTimeout(() => {
+        setCopySuccess(false);
+      }, 2000);
+    }
+  }, [copySuccess]);
+
+  const getButtonRef = (type: ShareTypeEnum) => {
+    return {
+      [ShareTypeEnum.PINTEREST]: pinterestButtonRef,
+      [ShareTypeEnum.FACEBOOK]: facebookShareButtonRef,
+      [ShareTypeEnum.TWITTER]: twitterShareButtonRef,
+    }[type];
+  };
+
+  const getImageURL = async () => {
+    const { image, error } = await getImage('allocation-share-card');
+
+    if (!error) {
+      const formData = new FormData();
+      formData.append('file', image);
+      const { data, error: uploadError } = await postUploadChart(formData);
+
+      if (!uploadError) {
+        setImageUrl(() => data?.url);
+
+        return data?.url;
       }
+
+      return null;
+    }
+
+    return null;
+  };
+
+  const getBlobImage = async (type: ShareTypeEnum) => {
+    const buttonRef = getButtonRef(type);
+
+    if (imageUrl) {
+      return buttonRef?.current?.click();
+    }
+
+    const imageURl = await getImageURL();
+
+    if (imageURl) {
+      return buttonRef?.current?.click();
     }
   };
 
-  useEffect(() => {}, []);
+  const handleCopyToClipboard = async () => {
+    if (imageUrl) {
+      navigator.clipboard.writeText(imageUrl);
+
+      return setCopySuccess(true);
+    }
+
+    const imageURl = await getImageURL();
+
+    if (imageURl) {
+      navigator.clipboard.writeText(imageURl);
+
+      return setCopySuccess(true);
+    }
+  };
 
   if (loading) {
     return <CircularSpinner />;
@@ -68,27 +135,52 @@ const ChartShareModal: React.FC<ChartShareModalProps> = ({ chartShareModal, char
             </div>
           </div>
           <div className='share-button-wrapper'>
-            <PinterestShareButton
-              beforeOnClick={getBlobImage}
-              url='www.github.com'
-              media='www.github.com'
+            <button
               className='share-button pinterest-share-button'
+              onClick={() => getBlobImage(ShareTypeEnum.PINTEREST)}
+            >
+              <PinterestIcon />
+              Pin It
+            </button>
+            <button className='share-button facebook-share-button' onClick={() => getBlobImage(ShareTypeEnum.FACEBOOK)}>
+              <FacebookIcon />
+              Share
+            </button>
+            <button className='share-button twitter-share-button' onClick={() => getBlobImage(ShareTypeEnum.TWITTER)}>
+              <TwitterIcon />
+              Tweet
+            </button>
+            <PinterestShareButton
+              ref={pinterestButtonRef}
+              url={imageUrl ? imageUrl : 'https://app.moneyminx.com/'}
+              media={imageUrl ? imageUrl : 'https://app.moneyminx.com/'}
+              className='d-none share-button pinterest-share-button'
             >
               <PinterestIcon />
               Pin It
             </PinterestShareButton>
-            <FacebookShareButton url='www.github.com' className='share-button facebook-share-button' disabled={!blob}>
+            <FacebookShareButton
+              ref={facebookShareButtonRef}
+              url={imageUrl ? imageUrl : 'https://app.moneyminx.com/'}
+              className='d-none share-button facebook-share-button'
+            >
               <FacebookIcon />
               Share
             </FacebookShareButton>
-            <TwitterShareButton url='www.github.com' className='share-button twitter-share-button' disabled={!blob}>
+            <TwitterShareButton
+              ref={twitterShareButtonRef}
+              url={imageUrl ? imageUrl : 'https://app.moneyminx.com/'}
+              className='d-none share-button twitter-share-button'
+            >
               <TwitterIcon />
               Tweet
             </TwitterShareButton>
           </div>
           <div className='divider'>or</div>
           <div className='outline-button-wrapper'>
-            <button className='btn-outline-primary mm-btn-animate'>Copy Image</button>
+            <button className='btn-outline-primary mm-btn-animate' onClick={handleCopyToClipboard}>
+              {copySuccess ? 'Copied' : 'Copy Image'}
+            </button>
             <button
               className='btn-outline-primary mm-btn-animate'
               onClick={() => df('allocation-share-card', 'my-awesome-allocation')}
