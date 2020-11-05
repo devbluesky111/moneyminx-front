77f2d01bdddc2b4b5d15a8f5b47f55cc1d6297d2
shrijan <shrijan00003@gmail.com>
2020-11-05 20:03:30 +0545

    mm-frontend#42-Develop-the-side-bar-menu:  used in allocation

--- src/allocation/views/allocation.tsx
+++ src/allocation/views/allocation.tsx
@@ -1,7 +1,5 @@
 import React, { useState } from 'react';
 
-import AppHeader from 'common/app.header';
-import FooterSection from 'auth/views/auth.footer';
 import useAllocation from 'allocation/hooks/useAllocation';
 import { AllocationsFilter } from 'allocation/allocation.enum';
 import CircularSpinner from 'common/components/spinner/circular-spinner';
@@ -9,9 +7,9 @@ import CircularSpinner from 'common/components/spinner/circular-spinner';
 import { AllocationProps } from '../allocation.type';
 import AllocationOverview from './allocation-overview';
 import AllocationSubNavigation from './allocation-sub-navigation';
+import AppLayout from 'layouts/app.layout';
 
 const Allocation: React.FC<AllocationProps> = () => {
-  const [openNav, setOpenNav] = useState<boolean>(false);
   const [filter, setFilter] = useState(AllocationsFilter.TYPE);
   const { fetching, allocations, error, allocationChartData } = useAllocation(filter);
 
@@ -23,12 +21,13 @@ const Allocation: React.FC<AllocationProps> = () => {
     setFilter(type);
   };
 
+  // FIXME: remove mm-setting
   return (
     <div className='mm-setting mm-allocation'>
-      <AppHeader toggleMenu={() => setOpenNav(!openNav)} />
-      <AllocationSubNavigation onTypeChange={handleTypeChange} filter={filter} />
-      <AllocationOverview allocations={allocations} chartData={allocationChartData} filter={filter} />
-      <FooterSection />
+      <AppLayout>
+        <AllocationSubNavigation onTypeChange={handleTypeChange} filter={filter} />
+        <AllocationOverview allocations={allocations} chartData={allocationChartData} filter={filter} />
+      </AppLayout>
     </div>
   );
 };
