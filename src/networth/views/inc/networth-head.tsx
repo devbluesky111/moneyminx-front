74abc38bdddd88f869e1a3d4c472266ef281b0e7
FashionCStar <fashioncstar@gmail.com>
2020-11-14 07:50:19 -0800

    networth fix6

--- src/networth/views/inc/networth-head.tsx
+++ src/networth/views/inc/networth-head.tsx
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Dropdown } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import { ReactComponent as DownloadExcel } from '../../../assets/images/allocation/download-excel.svg';
-import ReactHTMLTableToExcel from 'react-html-table-to-excel';
+// import ReactHTMLTableToExcel from 'react-html-table-to-excel';
+import { Account } from 'auth/auth.types';
+import { getAccount } from 'api/request.api';
+import { getRelativeDate } from 'common/moment.helper';
 
-const NetworthHead = () => {
+const NetworthHead = () => {  
+  const [currentAccount, setCurrentAccount] = useState<Account[]>();
+  useEffect(() => {
+    const fetchCurrentAccount = async () => {
+      const { data, error } = await getAccount();
+console.log("dddddddddddddddddddddddddddd");
+      if (!error) {
+        setCurrentAccount(data);
+      }
+    };
+    fetchCurrentAccount();
+  }, []);
   return (
     <div className='content-head'>
       <div className='container'>
@@ -19,44 +33,19 @@ const NetworthHead = () => {
                 </div>
                 <div className='dropdown-box'>
                   <ul className='pending'>
-                    <li>
-                      <Link to='/kkkk'>
-                        <div>
-                          <h5>Robinhood</h5>
-                          <span>10 days ago</span>
-                        </div>
-                        <div>$2,343</div>
-                      </Link>
-                    </li>
-                    <li>
-                      <Link to='/abc'>
-                        <div>
-                          <h5>Yieldstreet</h5>
-                          <span>12 days ago</span>
-                        </div>
-                        <div>$2,343</div>
-                      </Link>
-                    </li>
-                  </ul>
-                  <ul className='success'>
-                    <li>
-                      <Link to='/Robinhood'>
-                        <div>
-                          <h5>Robinhood</h5>
-                          <span>10 days ago</span>
-                        </div>
-                        <div>$2,343</div>
-                      </Link>
-                    </li>
-                    <li>
-                      <Link to='/yieldstreet'>
-                        <div>
-                          <h5>Yieldstreet</h5>
-                          <span>12 days ago</span>
-                        </div>
-                        <div>$2,343</div>
-                      </Link>
-                    </li>
+                    {currentAccount?.map((account, index) => {
+                      return (
+                        <li>
+                          <Link to='#'>
+                            <div>
+                              <h5>{account.accountName}</h5>
+                              <span>{getRelativeDate(account.balancesFetchedAt)}</span>
+                            </div>
+                            <div>${account.balance}</div>
+                          </Link>
+                        </li>
+                      )
+                    })}
                   </ul>
                 </div>
               </Dropdown.Menu>
@@ -112,11 +101,11 @@ const NetworthHead = () => {
         </div>
 
         <div className='right-box'>
-          {/* <button type='button' className='download-btn'>
+          <button type='button' className='download-btn'>
             <DownloadExcel />
             <span className='sm-hide'>Download</span> <span>CSV</span>
-          </button> */}
-          <ReactHTMLTableToExcel
+          </button>
+          {/* <ReactHTMLTableToExcel
             id='investment-table-xls-button'
             className='download-btn'
             table='table'
@@ -126,7 +115,7 @@ const NetworthHead = () => {
               <span><DownloadExcel />
               <span className='sm-hide'>Download</span> <span>CSV</span></span>
             }
-          />
+          /> */}
         </div>
       </div>
     </div>
