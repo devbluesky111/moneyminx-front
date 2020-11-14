1badfd4e078069f82c8f9a7cbcfe4af0742bfe03
hussein <h.yahfoufi@gmail.com>
2020-11-14 11:40:41 -0800

    build error

--- src/networth/views/inc/networth-filter.tsx
+++ src/networth/views/inc/networth-filter.tsx
@@ -18,7 +18,6 @@ import {
   setFilterTimeInterval,
   setFilterToDate,
 } from 'networth/networth.actions';
-import MMSelect from 'common/components/input/select.input';
 
 const NetworthFilter = () => {
   const dispatch = useNetworthDispatch();
