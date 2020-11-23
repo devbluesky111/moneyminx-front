1a5feaf9ef95ee20a98e2d3f64607dd8f8ead2a8
FashionCStar <fashioncstar@gmail.com>
2020-11-23 09:47:48 +0800

    remove blue on click

--- src/setting/inc/save-settings.tsx
+++ src/setting/inc/save-settings.tsx
@@ -8,7 +8,7 @@ interface SaveSettingProps {
 const SaveSettings: React.FC<SaveSettingProps> = ({ handleSave, status= false }) => {
   return (
     <div className='action-overlay'>
-      <button type='button' className='btn btn-primary btn-lg mm-button float-right' onClick={handleSave}>
+      <button type='button' className='btn btn-lg mm-button float-right' onClick={handleSave}>
         {status && <span className='spinner-grow spinner-grow-sm' role='status' aria-hidden='true'/>}
         <span className={'ml-1'}> {status ? 'Saving...' : 'Save changes'}</span>
       </button>
