2a0b7933aedc5597ef54be3d754098e6fb01565a
cleverjack <example@test.com>
2020-12-02 02:33:47 -0100

    add manual acc modal api integrated

--- src/auth/views/inc/manual-account.modal.tsx
+++ src/auth/views/inc/manual-account.modal.tsx
@@ -1,85 +1,143 @@
 import Form from 'react-bootstrap/Form';
 import React, { useState } from 'react';
-
-import { FormControl } from 'react-bootstrap';
+import { toast } from 'react-toastify';
+import { currencyArray } from 'common/currency-helper';
 import { Modal, ModalType } from 'common/components/modal';
-import { patchAllocationChartSettings } from 'api/request.api';
+import { getManualAccountType, postManualAccount } from 'api/request.api';
+import { appRouteConstants } from 'app/app-route.constant';
+import { useHistory } from 'react-router-dom';
+import CircularSpinner from 'common/components/spinner/circular-spinner';
 
 interface SettingModalProps {
     manualAccountModal: ModalType;
 }
 
-const initialData = {
+const initialValues = {
     yodleeAccountType: '',
     accountName: '',
-    balance: 0,
+    nickname: '',
+    balance: '',
     accountNumber: '',
     currency: '',
 };
 
 const ManualAccountModal: React.FC<SettingModalProps> = ({ manualAccountModal }) => {
-    const [data, setData] = useState(initialData);
+    const history = useHistory();
+    const [values, setValues] = useState(initialValues);
+    const [AccountTypes, setAccountTypes] = useState([]);
+    const [disabled, setDisabled] = useState<boolean>(true);
+    const [loading, setLoading] = useState<boolean>(false);
+    const curArr = currencyArray();
 
     const handleChange = (e: React.ChangeEvent<any>) => {
-        setData({ ...data, [e.target.name]: e.target.value });
-    };
-
-    const handleCancel = () => manualAccountModal.close();
-
-    const handleSubmit = async () => {
-
-        const { data: patchResponse, error: allocationError } = await patchAllocationChartSettings(data);
-
-        if (!allocationError) {
-            setData({ ...patchResponse });
-            manualAccountModal.close();
+        if (e.target.name === 'balance') {
+            let _float = parseFloat(e.target.value);
+            setValues({ ...values, [e.target.name]: _float });
+        } else {
+            setValues({ ...values, [e.target.name]: e.target.value });
         }
     };
+
+    const handleCancel = () => {
+        manualAccountModal.close();
+        setValues(initialValues);
+    }
+
+    const handleSubmit = async () => {
+        setLoading(true);
+        const { data: res, error: err } = await postManualAccount(values);
+        if (!err) {
+            console.log(res);
+            setValues(initialValues);
+            setLoading(false);
+            toast('Add Success', { type: 'success' });
+            manualAccountModal.close();
+            history.push(appRouteConstants.account.ACCOUNT.replace(':accountId', res.id));
+        } else {
+            console.log(err);
+            setValues(initialValues);
+            setLoading(false);
+            toast(' Add Failed', { type: 'error' });
+        }
+    };
+
+    const fetchAccountTypes = async () => {
+        const { data, error } = await getManualAccountType();
+        if (!error) {
+            setAccountTypes(data);
+        }
+    };
+
+    React.useEffect(() => {
+        fetchAccountTypes();
+    }, [])
+
+    React.useEffect(() => {
+        setValues(initialValues);
+    }, [manualAccountModal])
+
+    React.useEffect(() => {
+        for (let i = 0; i < Object.values(values).length; i++) {
+            if (!Object.values(values)[i]) {
+                setDisabled(true);
+                break;
+            }
+            setDisabled(false);
+        }
+    }, [values])
 
     return (
         <Modal {...manualAccountModal.props} title='Add Manual Accounts' size='lg' canBeClosed onClose={() => manualAccountModal.close()}>
             <div className='modal-wrapper mm-manual-account-modal'>
                 <span className='description'>With manual accounts you can track any asset and liablity that is not currently supported by our integration partners.</span>
-                <div className='mm-manual-account-modal__title'>
+                <div className='mm-manual-account-modal__title mt-3'>
                     <Form>
-                        <Form.Group controlId='ManualAccountForm.AccountName'>
-                            <Form.Label className='mm-manual-account-modal__sub-title'>Account Name</Form.Label>
-                            <Form.Control
-                                type='text'
-                                size='lg'
-                                placeholder='Sapphire Credit Card'
-                                onChange={handleChange}
-                                name='title'
-                                value={data.accountName}
-                            />
-                            <FormControl.Feedback type='invalid' className='feedback text-right'>
-                                Sapphire Credit Card
-                            </FormControl.Feedback>
-                        </Form.Group>
+                        <div className='row-set'>
+                            <Form.Group controlId='ManualAccountForm.AccountName' className='child'>
+                                <Form.Label className='mm-manual-account-modal__sub-title'>Account Name</Form.Label>
+                                <Form.Control
+                                    type='text'
+                                    placeholder='Sapphire Credit Card'
+                                    onChange={handleChange}
+                                    name='accountName'
+                                    value={values.accountName}
+                                />
+                            </Form.Group>
+                            <Form.Group controlId='ManualAccountForm.nickname' className='child'>
+                                <Form.Label className='mm-manual-account-modal__sub-title'>Nick Name</Form.Label>
+                                <Form.Control
+                                    type='text'
+                                    placeholder='Sapphire'
+                                    onChange={handleChange}
+                                    name='nickname'
+                                    value={values.nickname}
+                                    required
+                                />
+                            </Form.Group>
+                        </div>
                         <div className='row-set'>
                             <Form.Group controlId='ManualAccountForm.AccountType' className='child'>
                                 <Form.Label className='mm-manual-account-modal__sub-title'>Account Type</Form.Label>
                                 <Form.Control
                                     as="select"
-                                    name='description'
+                                    name='yodleeAccountType'
                                     onChange={handleChange}
-                                    value={data.yodleeAccountType}
                                 >
-                                    <option>1</option>
-                                    <option>2</option>
-                                    <option>3</option>
-                                    <option>4</option>
-                                    <option>5</option>
+                                    <option value=''>Select Account Type</option>
+                                    {AccountTypes.map((item, index) => (
+                                        <option key={index} value={item}>{item}</option>
+                                    ))}
                                 </Form.Control>
                             </Form.Group>
                             <Form.Group controlId='ManualAccountForm.AccountNumber' className='child'>
                                 <Form.Label className='mm-manual-account-modal__sub-title'>Account Number</Form.Label>
                                 <Form.Control
-                                    name='description'
+                                    name='accountNumber'
                                     type='number'
                                     placeholder='45432'
                                     onChange={handleChange}
-                                    value={data.yodleeAccountType}
+                                    value={values.accountNumber}
+                                    required
                                 />
                             </Form.Group>
                         </div>
@@ -87,21 +145,26 @@ const ManualAccountModal: React.FC<SettingModalProps> = ({ manualAccountModal })
                             <Form.Group controlId='ManualAccountForm.CurrentBalance' className='child'>
                                 <Form.Label className='mm-manual-account-modal__sub-title'>Current Balance</Form.Label>
                                 <Form.Control
-                                    name='description'
+                                    name='balance'
                                     type='number'
                                     placeholder='43,233'
                                     onChange={handleChange}
-                                    value={data.yodleeAccountType}
+                                    value={values.balance}
+                                    required
                                 />
                             </Form.Group>
                             <Form.Group controlId='ManualAccountForm.Currency' className='child'>
                                 <Form.Label className='mm-manual-account-modal__sub-title'>Currency</Form.Label>
                                 <Form.Control
-                                    name='description'
-                                    placeholder='USD'
+                                    as='select'
+                                    name='currency'
                                     onChange={handleChange}
-                                    value={data.yodleeAccountType}
-                                />
+                                >
+                                    <option value=''>Select Currency</option>
+                                    {curArr.map((item, index) => (
+                                        <option key={index} value={item}>{item}</option>
+                                    ))}
+                                </Form.Control>
                             </Form.Group>
                         </div>
                     </Form>
@@ -109,13 +172,19 @@ const ManualAccountModal: React.FC<SettingModalProps> = ({ manualAccountModal })
                         <button className='btn-outline-primary mm-btn-animate' onClick={handleCancel}>
                             Cancel
                         </button>
-                        <button className='mm-btn-animate mm-btn-primary' onClick={handleSubmit}>
+                        <button className='mm-btn-animate mm-btn-primary' onClick={handleSubmit} disabled={disabled}>
                             Add <span className='hide-sm'>Account</span>
+                            {loading &&
+                                <span className='spinner'>
+                                    <CircularSpinner />
+                                </span>
+                            }
                         </button>
+
                     </div>
                 </div>
             </div>
-        </Modal>
+        </Modal >
     );
 };
 
