e59a5e199ffd8b8000239593e24270c6f474d977
Muhammad Aftab Aslam <formanite.2000@gmail.com>
2020-12-06 08:06:56 +0100

    fix: updated route to "/subscription/review"

--- src/app/app.view.ts
+++ src/app/app.view.ts
@@ -1,6 +1,7 @@
 export { default as TokenExpired } from 'auth/views/expired-link';
 export { default as AccountSetting } from 'auth/views/account-settings';
 export { default as Subscription } from 'auth/views/subscription';
+export { default as SubscriptionReview } from 'auth/views/subscription-review';
 export { default as ConnectAccount } from 'auth/views/connect-account';
 export { default as ResetPassword } from 'auth/views/create-new-password';
 export { default as StripeSuccess } from 'setting/pages/stripe-checkout-success';
---
+++ src/auth/views/subscription-review.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import {AccountOverview} from '../../setting/pages/account-overview';
+
+
+const SubscriptionReview = () => {
+  return (
+    <section>
+      <div className='subscription-ended bottom py-5'>
+        <div className='container'>
+          <AccountOverview updateAccountsFlag={true}/>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default SubscriptionReview;
+
