42f3694ff8a8cc99c86099959e536291f78a1ce3
shrijan <shrijan00003@gmail.com>
2020-12-09 23:43:11 +0545

    mm-frontend#21: - custom component GALink added with onclick event trigger

---
+++ src/@mm/data/event-list.ts
@@ -0,0 +1,7 @@
+export const events = {
+  completeProfile: {
+    category: 'Profile',
+    action: 'Complete Profile',
+    label: 'Complete profile from networth page to settings profile tab',
+  },
+};
