97a7c6d1e40b0ec4b7973f6b691bc623bc12c569
hussein <h.yahfoufi@gmail.com>
2020-12-21 05:40:15 -0800

    Session Expired removed

--- src/auth/auth.route.tsx
+++ src/auth/auth.route.tsx
@@ -3,14 +3,13 @@ import { NotFound } from 'website/views';
 import { Switch, Redirect, Route } from 'react-router-dom';
 
 import { authRouteConstants } from './authRouteConstants';
-import { Login, Signup, ExpiredLink, SessionExpired, ConnectAccount, AccountSetting, CreateNewPassword } from './views';
+import { Login, Signup, ExpiredLink, ConnectAccount, AccountSetting, CreateNewPassword } from './views';
 
 const {
   LOGIN,
   SIGNUP,
   NOT_FOUND,
   EXPIRED_LINK,
-  SESSION_EXPIRED,
   CONNECT_ACCOUNT,
   ACCOUNT_SETTING,
   CREATE_NEW_PASSWORD,
@@ -19,7 +18,6 @@ const AuthRoute = () => {
   return (
     <Switch>
       <Route exact path={LOGIN} component={Login} />
-      <Route exact path={SESSION_EXPIRED} component={SessionExpired} />
       <Route exact path={CREATE_NEW_PASSWORD} component={CreateNewPassword} />
       <Route exact path={EXPIRED_LINK} component={ExpiredLink} />
       <Route exact path={CONNECT_ACCOUNT} component={ConnectAccount} />
