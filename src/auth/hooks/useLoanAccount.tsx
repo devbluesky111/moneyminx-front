636a5c3656e6d3cbe0b35175d038cfdefb87ba6a
shrijan <shrijan00003@gmail.com>
2020-12-23 13:00:50 +0545

    mm-frontend#583: - associated loan and loan accout error fixed - type for loan account added

--- src/auth/hooks/useLoanAccount.tsx
+++ src/auth/hooks/useLoanAccount.tsx
@@ -1,9 +1,15 @@
 import useFetch from 'common/hooks/useFetch';
 import { getLoanAccounts } from 'api/request.api';
 
+interface ILoanAccount {
+  id: number;
+  balance: number;
+  accountName: string;
+}
+
 const useLoanAccount = () => {
   const { res, loading } = useFetch(getLoanAccounts, {});
-  return { fetchingLoanAccount: loading, loanAccounts: res?.data as string[], loanAccountError: res?.error };
+  return { fetchingLoanAccount: loading, loanAccounts: res?.data as ILoanAccount[], loanAccountError: res?.error };
 };
 
 export default useLoanAccount;
